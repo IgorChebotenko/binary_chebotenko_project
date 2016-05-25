b1f5f347e90aba055584bdce80355d6504d50aa6
IgorChebotenko <igordredd@gmail.com>
2016-05-25 20:30:19 +0300

    add class Test

--- binary_chebotenko_project/Program.cs
+++ binary_chebotenko_project/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace binary_chebotenko_project
 {
+    class Test
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Binary");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Test test = new Test();
+            test.Test1();
+            test.Test2();
         }
     }
 }
